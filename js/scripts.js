e80ba6b5904e4b3b6fcddf6254adec3cce2f91cf
Phil Curran <pecurran@hotmail.com>
2021-11-13 21:55:44 -0800

    final tweaks before publication

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   // Biz Logic
-
   let score = 0;
 
   const ruby = (name, age) => {
@@ -17,7 +16,7 @@ $(document).ready(function() {
   };
 
   const cSharp = (name, age) => {
-    return `<p>Excellent choice, ${name}!  Your preferences indicate that C# would be a good fit for you!  Plus, at ${age} years of age, now is the perfect time!</p><p>Go <a href='https://www.python.org/' target='_blank'>here</a> to learn more about Python!</p>`
+    return `<p>Excellent choice, ${name}!  Your preferences indicate that C# would be a good fit for you!  Plus, at ${age} years of age, now is the perfect time!</p><p>Go <a href='https://www.python.org/' target='_blank'>here</a> to learn more about C#!</p>`
   };
 
 // UI Logic
@@ -34,19 +33,15 @@ $(document).ready(function() {
     $('#questionOne').fadeIn(1000);
     $('#q1o1').click(function() {
       score++;
-      console.log('Score: ', score);
     });
     $('#q1o2').click(function() {
       score = score + 2;
-      console.log('Score: ', score);
     });
     $('#q1o3').click(function() {
       score = score + 3;
-      console.log('Score: ', score);
     });
     $('#q1o4').click(function() {
       score = score + 4;
-      console.log('Score: ', score);
     });
   });
 
@@ -55,19 +50,15 @@ $(document).ready(function() {
     $('#questionTwo').fadeIn(1000);
     $('#q2o1').click(function() {
       score++;
-      console.log('Score: ', score);
     });
     $('#q2o2').click(function() {
       score = score + 2;
-      console.log('Score: ', score);
     });
     $('#q2o3').click(function() {
       score = score + 3;
-      console.log('Score: ', score);
     });
     $('#q2o4').click(function() {
       score = score + 4;
-      console.log('Score: ', score);
     });
   });
 
@@ -76,19 +67,15 @@ $(document).ready(function() {
     $('#questionThree').fadeIn(1000);
     $('#q3o1').click(function() {
       score++;
-      console.log('Score: ', score);
     });
     $('#q3o2').click(function() {
       score = score + 2;
-      console.log('Score: ', score);
     });
     $('#q3o3').click(function() {
       score = score + 3;
-      console.log('Score: ', score);
     });
     $('#q3o4').click(function() {
       score = score + 4;
-      console.log('Score: ', score);
     });
   });
 
@@ -97,31 +84,27 @@ $(document).ready(function() {
     $('#questionFour').fadeIn(1000);
     $('#q4o1').click(function() {
       score++;
-      console.log('Score: ', score);
     });
     $('#q4o2').click(function() {
       score = score + 2;
-      console.log('Score: ', score);
     });
     $('#q4o3').click(function() {
       score = score + 3;
-      console.log('Score: ', score);
     });
     $('#q4o4').click(function() {
       score = score + 4;
-      console.log('Score: ', score);
     });
   });
 
   const decider = (score, name, age) => {
-    if (score >= 3) {
-      return ruby(name, age);
-    } else if (score >=4 || score <= 7) {
-      return javaScript(name, age);
-    } else if (score >= 8 || score <= 11) {
-      return python(name, age);
-    } else {
+    if (score >= 13) {
       return cSharp(name, age);
+    } else if (score <= 12 || score >= 9) {
+      return python(name, age);
+    } else if (score <= 8 || score >= 5) {
+      return javascript(name, age);
+    } else {
+      return ruby(name, age);
     }
   };
 
@@ -136,19 +119,16 @@ $(document).ready(function() {
       $('#results').fadeIn(1000);
       const name = $("#userName").val();
       const age = $("#userAge").val();
-      console.log('User name is: ', name);
-      console.log('User age is: ', age);
       // biz logic
       let result = decider(score, name, age);
       $("#result").html(result);
       $('#reload').show();
       $('#reload').click(function() {
+        name = '';
+        age = '';
+        score = 0;
         location.reload();
+      });
     });
-    });
-
-
   });
-
-
 });
